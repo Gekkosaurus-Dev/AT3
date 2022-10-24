67411b9969489862192afed04c2fe657ecb03022
LaurenSUni <111717811+LaurenSUni@users.noreply.github.com>
2022-10-24 13:21:20 +1100

    moved player character to front

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -14,6 +14,7 @@ public class Movement : MonoBehaviour
     private Vector3 endPos;
     private bool posLock;
 
+
     public GameObject pacStudent;
     public Animator movementAnimator;
     public AudioSource soundEffects;
