f2f286981c86a6349e19ac51baad2f9b17618bc7
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2022-10-27 17:44:53 +1100

    animating sprites around title

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -9,6 +9,7 @@ public class Movement : MonoBehaviour
     private int arrayPos2;
     //private float startTime;
     private float duration;
+    private float speed;
     private float timeElapsed;
     private Vector3 startPos;
     private Vector3 endPos;
@@ -16,43 +17,37 @@ public class Movement : MonoBehaviour
 
     public GameObject obj;
     public Animator movementAnimator;
-    public AudioSource soundEffects;
-    public AudioClip step;
+    //public AudioSource soundEffects;
+    //public AudioClip step;
 
     // Start is called before the first frame update
     void Start()
     {
-      pos.Add(new Vector3(1.0f, -1.0f, 0.0f));
-      pos.Add(new Vector3(6.0f, -1.0f, 0.0f));
-      pos.Add(new Vector3(6.0f, -5.0f, 0.0f));
-      pos.Add(new Vector3(1.0f, -5.0f, 0.0f));
+      pos.Add(new Vector3(-13.0f, -1.0f, 0.0f));      pos.Add(new Vector3(-7.0f, -1.0f, 0.0f));
+
+      pos.Add(new Vector3(-7.0f, -4.0f, 0.0f));       pos.Add(new Vector3(-13.0f, -4.0f, 0.0f));
       arrayPos = 0;
       arrayPos2 = 1;
-      obj.transform.position = pos[arrayPos];
-      startPos = pos[arrayPos];
+      //obj.transform.position = pos[arrayPos];
+      startPos = obj.transform.position;
       endPos = pos[arrayPos2];
       //startTime = Time.time;
-      duration = 2.0f;
-      playSound();
-
+      //duration = 2.0f;
+      speed = 2.0f;
+      //playSound();
       movementAnimator.SetInteger("Direction",arrayPos2);
     }
-
-    void playSound(){
-      soundEffects.Play();
-      Invoke("playSound", (step.length + 0.5f));
-    }
-
-    // Update is called once per frame
+    // void playSound(){
+    //   soundEffects.Play();
+    //   Invoke("playSound", (step.length + 0.5f));
+    // }
     void Update()
     {
       float distance = Vector3.Distance(obj.transform.position, endPos);
-        //Debug.Log("distance to point = " + distance);
-        //Debug.Log("current position" + obj.transform.position);
-        //Debug.Log("end position" + endPos);
         if (distance > 0.01f){
-        obj.transform.position = Vector3.Lerp(startPos, endPos, (timeElapsed/duration));
-        timeElapsed += Time.deltaTime;
+        //obj.transform.position = Vector3.Lerp(startPos, endPos, (timeElapsed/duration));
+        obj.transform.position = Vector3.MoveTowards(transform.position, endPos, Time.deltaTime * speed);
+        //timeElapsed += Time.deltaTime;
       }
       else if ((distance <= 0.01f)){
         //Debug.Log("point reached");
@@ -67,7 +62,7 @@ public class Movement : MonoBehaviour
         obj.transform.position = pos[arrayPos];
         startPos = pos[arrayPos];
         endPos = pos[arrayPos2];
-        timeElapsed = 0;
+        //timeElapsed = 0;
         movementAnimator.SetInteger("Direction",arrayPos2);
       }
     }
