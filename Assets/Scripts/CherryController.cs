6f088f393e14eb1f9677c3ac3cc64be271852f78
LaurenSUni <lauren.szmandiuk@student.uts.edu.au>
2022-10-30 22:40:15 +1100

    movement is sort of working for the cherry

--- Assets/Scripts/CherryController.cs
+++ Assets/Scripts/CherryController.cs
@@ -24,38 +24,58 @@ public class CherryController : MonoBehaviour
     private float speed;
     private float endX;
     private float endY;
+    private float startX;
+    private float startY;
     private float distanceTo;
     private Vector3 endPos;
     private GameObject newCherry;
 
     void Start()
     {
-      speed = 5.0f;
-      InvokeRepeating("SpawnCherry", 1.0f, 10.0f);
+      speed = 10.0f;
+      InvokeRepeating("SpawnCherry", 10.0f, 10.0f);
     }
 
     void SpawnCherry(){
       side = Random.Range(1,5);
       if (side == 1){ //left
         //Debug.Log("spawned on left");
-        position = new Vector3(-16.0f, Random.Range(-31.0f, 3.0f), 0.0f);
+        startX = -16.0f;
+        startY = Random.Range(-31.0f, 3.0f);
+        position = new Vector3(startX, startY, 0.0f);
+        newCherry = Instantiate(cherry, position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        endX = 43.0f;
+        endY = newCherry.transform.position.y - ((14.0f - newCherry.transform.position.y) * 2);
       }
       else if (side == 2){ //right
         //Debug.Log("spawned on right");
-        position = new Vector3(43.0f, Random.Range(-31.0f, 3.0f), 0.0f);
+        startX = 43.0f;
+        startY = Random.Range(-31.0f, 3.0f);
+        position = new Vector3(startX, startY, 0.0f);
+        newCherry = Instantiate(cherry, position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        endX = -16.0f;
+        endY = newCherry.transform.position.y - ((14.0f - newCherry.transform.position.y) * 2);
       }
       else if (side == 3){ //top
         //Debug.Log("spawned on top");
-        position = new Vector3(Random.Range(-16.0f, 43.0f), 3.0f, 0.0f);
+        startX = Random.Range(-16.0f, 43.0f);
+        startY = 3.0f;
+        position = new Vector3(startX, startY, 0.0f);
+        newCherry = Instantiate(cherry, position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        endX = newCherry.transform.position.x + ((13.5f - newCherry.transform.position.x) * 2);
+        endY = -31.0f;
       }
       else{ //bottom
         //Debug.Log("spawned on bottom");
-        position = new Vector3(Random.Range(-16.0f, 43.0f), -31.0f, 0.0f);
+        startX = Random.Range(-16.0f, 43.0f);
+        startY =  -31.0f;
+        position = new Vector3(startX, startY, 0.0f);
+        newCherry = Instantiate(cherry, position, Quaternion.Euler(new Vector3(0, 0, 0)));
+        endX = newCherry.transform.position.x + ((13.5f - newCherry.transform.position.x) * 2);
+        endY = 3.0f;
       }
-      newCherry = Instantiate(cherry, position, Quaternion.Euler(new Vector3(0, 0, 0)));
+
       //center = 13.5f, -14.0f, 0.0f
-      endX = ((13.5f - newCherry.transform.position.x) * 2);
-      endY = ((14.0f - newCherry.transform.position.y) * 2);
       endPos = new Vector3(endX, endY, 0.0f);
 
     }
@@ -64,9 +84,12 @@ public class CherryController : MonoBehaviour
       if (newCherry != null){
         distanceTo = Vector3.Distance(newCherry.transform.position, endPos);
         Debug.Log("targetPos = " + endPos);
-      if (distanceTo > 0.01f){
-        newCherry.transform.position = Vector3.MoveTowards(newCherry.transform.position, endPos, Time.deltaTime * speed);
+        if (distanceTo > 0.01f){
+          newCherry.transform.position = Vector3.MoveTowards(newCherry.transform.position, endPos, Time.deltaTime * speed);
+        }
+        else{
+          Destroy(newCherry);
+        }
       }
     }
-  }
 }
